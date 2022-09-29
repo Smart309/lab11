bf7bfe1be2456f2445f52790ed14421bea62bc76
Smart309 <pattharapon_takham@cmu.ac.th>
2022-09-29 23:01:41 +0700

    "finish"

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -8,31 +8,58 @@ import { checkToken } from "../../../../backendLibs/checkToken";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     //check token
-
+    const username = checkToken(req);
+    if (!username) {
+      return res.status(401).json({
+        ok: false,
+        message: "You don't have permission to access this api",
+      });
+    }
     //get roomId from url
     const roomId = req.query.roomId;
 
     const rooms = readChatRoomsDB();
 
     //check if roomId exist
-
+    const roomIdx = rooms.find((x) => x.roomId === roomId);
+    if (roomIdx === undefined) {
+      res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      res.json({ ok: true, message: roomIdx.messages });
+    }
     //find room and return
     //...
   } else if (req.method === "POST") {
     //check token
-
+    const username = checkToken(req);
+    if (!username) {
+      return res.status(401).json({
+        ok: false,
+        message: "You don't have permission to access this api",
+      });
+    }
     //get roomId from url
     const roomId = req.query.roomId;
     const rooms = readChatRoomsDB();
-
+    const text = req.body.text;
     //check if roomId exist
-
+    const roomIdx = rooms.find((x) => x.roomId === roomId);
+    if (roomIdx === undefined) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
+    const newId = uuidv4();
+    const newMeg = {
+      messageId: newId,
+      text: text,
+      username: username.username,
+    };
     //validate body
-    if (typeof req.body.text !== "string" || req.body.text.length === 0)
+    if (typeof req.body.text !== "string" || req.body.text.length === 0) {
       return res.status(400).json({ ok: false, message: "Invalid text input" });
-
+    }
     //create message
-
+    roomIdx.messages.push(newMeg);
     writeChatRoomsDB(rooms);
+    return res.json({ ok: true, message: newMeg });
   }
 }
--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -5,17 +5,49 @@ import bcrypt from "bcrypt";
 export default function login(req, res) {
   if (req.method === "POST") {
     const { username, password } = req.body;
-
+    if (
+      typeof username !== "string" ||
+      username.length === 0 ||
+      typeof password !== "string" ||
+      password.length === 0
+    ) {
+      return res
+        .status(404)
+        .json({ ok: true, message: "Username or password cannot be empty" });
+    }
     //validate body
 
     const users = readUsersDB();
 
     //find users with username, password
+    const foundUser = users.find(
+      (x) => x.username === username && bcrypt.compareSync(password, x.password)
+    );
+    if (!foundUser) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid Username or Password" });
+    } else {
+      const secret = process.env.JWT_SECRET;
 
-    const secret = process.env.JWT_SECRET;
-
-    //sign token
-
-    //return response
+      //sign token
+      const token = jwt.sign(
+        {
+          username: foundUser.username,
+          isAdmin: foundUser.isAdmin,
+        },
+        secret,
+        {
+          expiresIn: "1h",
+        }
+      );
+      //return response
+      return res.json({
+        ok: true,
+        username: foundUser.username,
+        isAdmin: foundUser.isAdmin,
+        token,
+      });
+    }
   }
 }
